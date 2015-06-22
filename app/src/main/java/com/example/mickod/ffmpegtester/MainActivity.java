e35b0f4c86c628e444988b7cee615e3beadcc2e3
Mick <mick.odoherty@amodtech.com>
2015-06-22 22:38:01 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mickod/ffmpegtester/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.mickod.ffmpegtester;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends AppCompatActivity
+        implements  FfmpegTestMainFragment.OnFragmentInteractionListener,
+                    AboutFragment.OnFragmentInteractionListener {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Set the toolbar as an action bar
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+
+        //Add the fragment for the main test UI if this the first creation (i.e. it is not just after a
+        //orientation change
+        if(savedInstanceState == null) {
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            FfmpegTestMainFragment mainTestFragment = new FfmpegTestMainFragment();
+            fragmentTransaction.add(R.id.main_activity_frag_container, mainTestFragment);
+            fragmentTransaction.commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //Settings
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        //About
+        if (id == R.id.action_about) {
+
+            //chnage to the about fragment
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            AboutFragment aboutFragment = new AboutFragment();
+            fragmentTransaction.replace(R.id.main_activity_frag_container, aboutFragment);
+            fragmentTransaction.commit();
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        //This method is a listener for events from the FfmpegTestMainFragment
+
+    }
+}
